9cb28587ae6a99ac7336d8a654cc2574628a7c8c
NathanBwire <sebbynathan7@gmail.com>
2024-04-04 20:35:01 +0300

    structure

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocknumber){
+  if  (blocknumber>=42){
+    return blocknumber-42;
+  }else{
+    return 42-blocknumber
+  }
+}
+function distanceFromHqInFeet (blocknumber){
+  return distanceFromHqInBlocks(blocknumber)*264;
+  }
+  function distanceTravelledInFeet (start,destination){
+     if (start>destination) {
+        return(start-destination)*264;
+      }else{
+          return(destination-start)*264;}
+      }
+ function calculatesFarePrice(start,destination){
+  const distance =distanceTravelledInFeet(start,destination);
+  if (distance <=400){
+    return 0;
+  }else if (distance >400 && distance <= 2000){
+    return.02 *(distance -400);
+  }else if (distance >2000 && distance <= 2500){
+    return 25;
+  }else{
+    return`cannot travel that far`
+  }
+  }
+      
+    
+
+
+  
+
+distanceFromHqInBlocks(90)
+console.log(distanceFromHqInBlocks(90))
+distanceFromHqInFeet(20)
+console.log(distanceFromHqInFeet(20))
